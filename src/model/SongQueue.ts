4e809351587ac6380859681e07391540bf883a0e
Sleepingtree <ajgrabow@gmail.com>
2023-02-20 21:23:55 -0500

    bruh

--- src/model/SongQueue.ts
+++
@@ -1,9 +0,0 @@
-export default class SongQueueItem{
-    url: string;
-    title: string;
-
-    constructor(url: string, title: string){
-        this.url = url;
-        this.title = title;
-    }
-}
